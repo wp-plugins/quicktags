4f05b25ff1b7842f8a27fb4d680e5f20aba089c2
rafdizzle86 <rafdizzle86@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-07 02:26:09 +0000

    going back to previous commit code
    git-svn-id: https://plugins.svn.wordpress.org/quicktags/tags/0.0.2@621932 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- quicktags.php
+++ quicktags.php
@@ -33,7 +33,6 @@ if ( !class_exists("quicktags") ) {
 		
 		static function enqueueJS(){
 			wp_enqueue_script('jquery');
-			wp_enqueue_script('jquery-ui-autocomplete');			
 		}
 		
 	}//end quicktags class
